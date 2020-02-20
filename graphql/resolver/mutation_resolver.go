b43340d318eb9c77a13d4f0665981dbaa52ffddf
Carlos Guzmán <cguzman@xmltravelgate.com>
2020-02-20 15:56:26 +0100

    user mutation resolvers

--- graphql/resolver/mutation_resolver.go
+++ graphql/resolver/mutation_resolver.go
@@ -2,10 +2,43 @@ package resolver
 
 import (
 	"context"
+	"github.com/guzmanweb/graphql-go"
+	"labx-graphql-go-todo/model"
 )
 
-func (r *TodoMutationResolver) Test(ctx context.Context) *string {
-	x := "test"
-	return &x
+type CreateUserInput struct {
+	Name string
 }
 
+type UpdateUserInput struct {
+	ID 		graphql.ID
+	Name 	string
+}
+
+type DeleteUserInput struct {
+	ID graphql.ID
+}
+
+func (r *TodoMutationResolver) CreateUser(ctx context.Context, args *struct {
+	Input *CreateUserInput
+}) *UserResolver {
+	return &UserResolver{u:model.User{
+		ID:   "u14",
+		Name: args.Input.Name,
+	}}
+}
+
+func (r *TodoMutationResolver) UpdateUser(ctx context.Context, args *struct {
+	Input *UpdateUserInput
+}) *UserResolver {
+	return &UserResolver{u:model.User{
+		ID:   string(args.Input.ID),
+		Name: args.Input.Name,
+	}}
+}
+
+func (r *TodoMutationResolver) DeleteUser(ctx context.Context, args *struct {
+	Input *DeleteUserInput
+}) *UserResolver {
+	return &UserResolver{u:model.User{}}
+}
